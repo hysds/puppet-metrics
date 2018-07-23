407d7622780ccc881b31e0ab245000b8116a3401
Gerald Manipon <pymonger@gmail.com>
2018-07-23 22:46:56 +0000

    fix issue with failure to import dashboards
    Ensure ES is running.
    Ensure import API call is authenticated.

--- templates/import_dashboards.sh
+++ templates/import_dashboards.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
+set -ex
+
+# start up elasticsearch
+systemctl start elasticsearch
 
 # start up kibana
+/tmp/wait-for-it.sh localhost:9200 -s -t 60 -- echo "ElasticSearch is running"
 sudo -u ops /home/<%= @user %>/kibana/bin/kibana &
 #echo "after kibana startup: $?"
 
 # import dashboards
-/tmp/wait-for-it.sh localhost:5601 -s -t 60 -- curl -XPOST --fail localhost:5601/api/kibana/dashboards/import -H 'kbn-xsrf:true' -H 'Content-type:application/json' -d @/tmp/worker_metrics.json
-#echo "after first import: $?"
-/tmp/wait-for-it.sh localhost:5601 -s -t 60 -- curl -XPOST --fail localhost:5601/api/kibana/dashboards/import -H 'kbn-xsrf:true' -H 'Content-type:application/json' -d @/tmp/job_metrics.json
+/tmp/wait-for-it.sh localhost:5601 -s -t 60 -- echo "Kibana is running"
+curl --user <%= @es_user %>:<%= @es_password %> -XPOST --fail \
+  localhost:5601/api/kibana/dashboards/import -H 'kbn-xsrf:true' \
+  -H 'Content-type:application/json' -d @/tmp/worker_metrics.json
+curl --user <%= @es_user %>:<%= @es_password %> -XPOST --fail \
+  localhost:5601/api/kibana/dashboards/import -H 'kbn-xsrf:true' \
+  -H 'Content-type:application/json' -d @/tmp/job_metrics.json
 #echo "after second import: $?"
-sleep 5
+sleep 10
 
 # terminate kibana
 PROC_ID=$(cat /home/<%= @user %>/metrics/run/kibana.pid)
