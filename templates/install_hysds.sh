1356f974634571a7fe4be1fcdc63be9176311fc0
Gerald Manipon <pymonger@gmail.com>
2019-05-26 18:31:46 +0000

    install hysds using hysds-framework

--- templates/install_hysds.sh
+++ templates/install_hysds.sh
@@ -1,56 +1,24 @@
 #!/bin/bash
-
-METRICS_DIR=<%= @metrics_dir %>
-
-
-## install elasticsearch-head dependencies
-#if [ ! -e "$HOME/node/bin/grunt" ]; then
-#  cd $HOME/elasticsearch-head
-#  npm install
-#  cd -
-#fi
+BASE_PATH=$(dirname "${BASH_SOURCE}")
+BASE_PATH=$(cd "${BASE_PATH}"; pwd)
 
 
-# create virtualenv if not found
-if [ ! -e "$METRICS_DIR/bin/activate" ]; then
-  /opt/conda/bin/virtualenv --system-site-packages $METRICS_DIR
-  echo "Created virtualenv at $METRICS_DIR."
+# usage
+usage() {
+  echo "usage: $0 <release tag>" >&2
+}
+
+
+# check usage
+if [ $# -ne 1 ]; then
+  usage
+  exit 1
 fi
+release=$1
 
 
-# source virtualenv
-source $METRICS_DIR/bin/activate
-
-
-# install latest pip and setuptools
-pip install -U pip
-pip install -U setuptools
-
-
-# force install supervisor
-if [ ! -e "$METRICS_DIR/bin/supervisord" ]; then
-  #pip install --ignore-installed supervisor
-  pip install --ignore-installed git+https://github.com/Supervisor/supervisor
-fi
-
-
-# create etc directory
-if [ ! -d "$METRICS_DIR/etc" ]; then
-  mkdir $METRICS_DIR/etc
-fi
-
-
-# create log directory
-if [ ! -d "$METRICS_DIR/log" ]; then
-  mkdir $METRICS_DIR/log
-fi
-
-
-# create run directory
-if [ ! -d "$METRICS_DIR/run" ]; then
-  mkdir $METRICS_DIR/run
-fi
-
+# print out commands and exit on any errors
+set -ex
 
 # set oauth token
 OAUTH_CFG="$HOME/.git_oauth_token"
@@ -62,84 +30,22 @@ else
 fi
 
 
-# create ops directory
-OPS="$METRICS_DIR/ops"
-if [ ! -d "$OPS" ]; then
-  mkdir $OPS
-fi
-
-
-# export latest prov_es package
-cd $OPS
-PACKAGE=prov_es
-if [ ! -d "$OPS/$PACKAGE" ]; then
+# clone hysds-framework
+cd $HOME
+PACKAGE=hysds-framework
+if [ ! -d "$HOME/$PACKAGE" ]; then
   git clone ${GIT_URL}/hysds/${PACKAGE}.git
 fi
-cd $OPS/$PACKAGE
-pip install -e .
-if [ "$?" -ne 0 ]; then
-  echo "Failed to run 'pip install -e .' for $PACKAGE."
-  exit 1
+cd $HOME/$PACKAGE
+if [ "$release" = "develop" ]; then
+  ./install.sh -d metrics
+else
+  ./install.sh -r $release metrics
 fi
 
 
-# export latest osaka package
-cd $OPS
-GITHUB_REPO=osaka
-PACKAGE=osaka
-if [ ! -d "$OPS/$PACKAGE" ]; then
-  git clone ${GIT_URL}/hysds/${PACKAGE}.git
-fi
-cd $OPS/$PACKAGE
-pip install -U pyasn1
-pip install -U pyasn1-modules
-pip install -U python-dateutil
-pip install -e .
-if [ "$?" -ne 0 ]; then
-  echo "Failed to run 'pip install -e .' for $PACKAGE."
-  exit 1
-fi
+METRICS_DIR=<%= @metrics_dir %>
 
 
-# export latest hysds_commons package
-cd $OPS
-PACKAGE=hysds_commons
-if [ ! -d "$OPS/$PACKAGE" ]; then
-  git clone ${GIT_URL}/hysds/${PACKAGE}.git
-fi
-cd $OPS/$PACKAGE
-pip install -e .
-if [ "$?" -ne 0 ]; then
-  echo "Failed to run 'pip install -e .' for $PACKAGE."
-  exit 1
-fi
-
-
-# export latest hysds package
-cd $OPS
-PACKAGE=hysds
-if [ ! -d "$OPS/$PACKAGE" ]; then
-  git clone ${GIT_URL}/hysds/${PACKAGE}.git
-fi
-cd $OPS/$PACKAGE/third_party/celery-v4.2.1
-pip install -e .
-cd $OPS/$PACKAGE
-pip install -e .
-if [ "$?" -ne 0 ]; then
-  echo "Failed to run 'pip install -e .' for $PACKAGE."
-  exit 1
-fi
-
-
-# export latest sciflo package
-cd $OPS
-PACKAGE=sciflo
-if [ ! -d "$OPS/$PACKAGE" ]; then
-  git clone ${GIT_URL}/hysds/${PACKAGE}.git
-fi
-cd $OPS/$PACKAGE
-pip install -e .
-if [ "$?" -ne 0 ]; then
-  echo "Failed to run 'pip install -e .' for $PACKAGE."
-  exit 1
-fi
+# source virtualenv
+source $METRICS_DIR/bin/activate
